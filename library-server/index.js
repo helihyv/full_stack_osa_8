6e4f1bda61a99f06fae71da1195204bc426ba0d7
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2019-03-25 11:42:28 +0200

    Tehtävä 8.2 valmis.

--- library-server/index.js
+++ library-server/index.js
@@ -84,9 +84,20 @@ const books = [
 ]
 
 const typeDefs = gql`
+
+  type Book {
+
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
@@ -94,7 +105,8 @@ const resolvers = {
   Query: {
 
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
