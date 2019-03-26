3b75f9fecbd5d84197ab198b5cf91c3666081a90
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2019-03-26 08:51:47 +0200

    Tehtävä 8.3 valmis.

--- library-server/index.js
+++ library-server/index.js
@@ -94,10 +94,19 @@ const typeDefs = gql`
     genres: [String!]!
 
   }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -106,8 +115,13 @@ const resolvers = {
 
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
-  }
+    allBooks: () => books,
+    allAuthors: () => authors,
+    },
+
+  Author: {
+    bookCount: (root) => books.filter((book) => book.author === root.name).length  
+  }  
 }
 
 const server = new ApolloServer({
