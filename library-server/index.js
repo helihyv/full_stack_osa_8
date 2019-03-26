3579834c935b40c830689b36dcaa501debd0dd30
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2019-03-26 09:07:15 +0200

    Tehtävä 8.4 valmis.

--- library-server/index.js
+++ library-server/index.js
@@ -105,7 +105,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -115,7 +115,12 @@ const resolvers = {
 
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter((book) => book.author === args.author)
+      }
+      return books
+    },
     allAuthors: () => authors,
     },
 
